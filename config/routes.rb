cd2dbf5384b3947ed5c1de423cf7d6ecec432f97
nguyenquocthong <quocthong9592@gmail.com>
2016-10-19 14:28:59 +0700

    Login-Logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/pages/*page",to: "pages#show"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   
-  resources :users, only: :show
+  resources :users, only: [:show, :create, :new]
 end
