808a4b07cd65aefaeabb96ca7ca13702c494b6fc
nhocbangchu95 <duchv2307@gmail.com>
2016-10-20 08:48:38 +0700

    Config for show list book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+  name = Faker::Name.name
+  Category.create!(
+    category_name: name
+  )
+end
+
+categories = Category.take(5)
+30.times do
+  title = Faker::Book.title
+  author = Faker::Book.author
+  description = Faker::Lorem.characters(100)
+  date = Faker::Date.between(2.days.ago, Date.today)
+  number_page = Faker::Number.between(1, 100)
+  categories.each{ |cat|
+    cat.books.create!(title: title,author: author,
+      publish_date: date, number_of_page: number_page,description: description)
+  }
+end
