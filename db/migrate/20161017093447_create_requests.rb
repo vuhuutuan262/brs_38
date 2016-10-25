2401645b5e37be530282a1b9fc8becef542ddcf7
nguyenquocthong <quocthong9592@gmail.com>
2016-10-25 11:08:44 +0700

    user-send-requests

--- db/migrate/20161017093447_create_requests.rb
+++ db/migrate/20161017093447_create_requests.rb
@@ -1,8 +1,9 @@
 class CreateRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :requests do |t|
-      t.boolean :is_approved
       t.string :book_name
+      t.string :author
+      t.string :content
       t.references :user, foreign_key: true
 
       t.timestamps
